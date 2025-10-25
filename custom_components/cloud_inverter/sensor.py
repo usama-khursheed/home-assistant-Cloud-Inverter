865908fa92b6624cc7c52ca468f79a101a1e8386
Usama Khan <user01usama@gmail.com>
2025-10-25 08:52:36 +0500

    Create sensor.py

---
+++ custom_components/cloud_inverter/sensor.py
@@ -0,0 +1,22 @@
+"""Sensor platform for Cloud Inverter integration."""
+import logging
+from typing import Any, Optional
+
+from homeassistant.components.sensor import (
+    SensorEntity,
+    SensorStateClass,
+    SensorDeviceClass,
+)
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import (
+    UnitOfPower,
+    UnitOfEnergy,
+    UnitOfTemperature,
+    UnitOfElectricPotential,
+    UnitOfElectricCurrent,
+    UnitOfFrequency,
+    PERCENTAGE,
+)
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.update_
